5d7a32e2f0c537aa4df13b89a092182a38008ce6
Eyal Dayan <yuuul.dayan@gmail.com>
2013-06-09 12:28:16 +0300

    Finds the minimum
    TODO: find the maximum, connect 9 signals to the entity

--- median.vhd
+++ median.vhd
@@ -17,24 +17,45 @@ ENTITY Median IS
 			num9 	: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 			num10 	: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 			num11 	: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
-			num12 	: IN 	STD_LOGIC_VECTOR( 31 DOWNTO 0 );
 			
 			dataOut 	: OUT 	STD_LOGIC_VECTOR( 31 DOWNTO 0 ) );
 END Median;
 
+
+
 ARCHITECTURE behavior OF Median IS
-	
-	min12,min34,min56,min78,min910,min11 :STD_LOGIC_VECTOR( 31 DOWNTO 0 );
-	min1234,min3456,min5678,min78910,min91011 :STD_LOGIC_VECTOR( 31 DOWNTO 0 );
-	min123456,min345678,min5678910,min7891011 : STD_LOGIC_VECTOR( 31 DOWNTO 0 );
-	
-	-- if it's smaller or larger than 6 numbers - discard 
-	--smaller than exactly 5 numbers- keep
-	
-	
-	
+signal anum1,anum2,anum3,anum4,anum5,anum6,anum7,anum8,anum9,anum10,anum11 :	STD_LOGIC_VECTOR( 31 DOWNTO 0  );
+
 begin
 	
+   
+   anum1 <= num1;
+   anum2 <= num2;
+   anum3 <= num3;
+   anum4 <= num4;
+   anum5 <= num5;
+   anum6 <= num6;
+   anum7 <= num7;
+   anum8 <= num8;
+   anum9 <= num9;
+   anum10 <= num10;
+   anum11 <= num11;
+   
+   
+   anum10 <= anum10 when anum10 < anum11 else anum11 ;
+	anum9 <= anum9 when anum9 < anum10 else anum10 ;
+   anum8 <= anum8 when anum8 < anum9 else anum9 ;
+   anum7 <= anum7 when anum7 < anum8 else anum8 ;
+   anum6 <= anum6 when anum6 < anum7 else anum7 ;
+   anum5 <= anum5 when anum5 < anum6 else anum6 ;
+   anum4 <= anum4 when anum4 < anum5 else anum5 ;
+	anum3 <= anum3 when anum3 < anum4 else anum4 ;
+   anum2 <= anum2 when anum2 < anum3 else anum3 ;
+   anum1 <= anum1 when anum1 < anum2 else anum2 ;
+
+   
+   
+  
 
 
 
